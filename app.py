d3cd573df21342a38bff3fe4e62b1f914d37341f
yusif <yusif@example.com>
2024-05-29 21:33:45 +0400

    last

--- app.py
+++ app.py
@@ -1,50 +1,20 @@
-from flask import Flask, redirect, request, render_template, abort,session
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
+from flask import Flask
+from flask import  render_template 
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@127.0.0.1:3306/mydb'
 app.config['SECRET_KEY'] = 'our_project'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-admin = Admin(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 from models import *
-from extensions import *
+from extentions import *
 from controllers import *
 
-class SecureModelView(ModelView):
-    def is_accessible(self):
-        if "logged_in" in session:
-            return True
-        else:
-            abort(403)
-
-admin.add_view(SecureModelView(User, db.session))
-admin.add_view(SecureModelView(Kart, db.session))
-admin.add_view(SecureModelView(Imkan, db.session))
-admin.add_view(SecureModelView(Sans, db.session))
-admin.add_view(SecureModelView(Tarif, db.session))
 
 
-
-
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        if request.form.get("username") == "admin" and request.form.get("password") == "admin1234":
-            session['logged_in']=True
-            return redirect('/admin/')
-        else:
-            return render_template("login.html", failed = True)
-    return render_template("login.html")
-    
-@app.route('/logout/')
-def logout():
-    session.clear()
-    return 'Logged out.'
 
 if __name__ == '__main__':
-    db.init_app(app)
-    migrate.init_app(app, db)
-    app.run(port=5000, debug=True)
+    app.init_app(app)
+    app.init_app(migrate)
+    app.run(port=5000,debug=True)
